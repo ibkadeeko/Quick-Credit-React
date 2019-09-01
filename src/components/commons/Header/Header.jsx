b2dac76724174d3b1301d14e7ab39c401ec801f9
Ibukunoluwa Adeeko <ibukunadeeko@gmail.com>
2019-09-01 12:36:07 +0100

    setup menu toggle

--- src/components/commons/Header/Header.jsx
+++ src/components/commons/Header/Header.jsx
@@ -4,17 +4,31 @@ import React, { Component } from 'react';
 import './Header.scss';
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hide: true
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      hide: !this.state.hide
+    });
+  }
+
   render() {
+    const showMenu = this.state.hide ? 'menu' : '';
     return (
       <header>
         <div className="container main-nav">
           <div className="logo">
               <a href="#">quick credit</a>
           </div>
-          <nav className="menu">
+          <nav className={showMenu}>
             <a className="login_btn btn-green" href="./html/login.html">sign in &#10230;</a>
           </nav>
-          <div className="menu-toggle">
+          <div className="menu-toggle" onClick={this.toggleMenu}>
               <a className="toggle-nav" href="#">&#9776;</a>
           </div>
         </div>
